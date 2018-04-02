f5d9c4542e66f4aca995187a0494bd1b9a1dc55a
Amy Kapernick <amy@aimhigherwebdesign.com>
2018-04-02 09:21:05 +0800

    added info modal

--- src/js/layouts/home.js
+++ src/js/layouts/home.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import Typed from 'typed.js';
 
 //Resources
-// import '../../scss/layouts/home.scss';
+import {Info, Twitter, Globe, X} from 'react-feather';
 import Adjectives from '../data/adjectives.js';
 
 class Meta extends Component {
@@ -79,7 +79,43 @@ export class Home extends Component {
 			<Fragment>
 				<h1>I think you're <span ref={(el) => {this.el = el;}} id="adjective"></span></h1>
 				<button onClick={() => this.newWord(this.generateNum())}>Not quite right? Try a different word!</button>
+				<About />
 			</Fragment>
 		);
 	}
 }
+
+const About = () => (
+	<div className="about">
+		<a href="#about" title="Link to display the information page">
+			<Info />
+		</a>
+		<div id="about">
+			<a href="#" className="close"><X /></a>
+			<h2>About this project</h2>
+			<p>This seems pretty cool, and we can put text here about stuff</p>
+			<p>This is based off the list of words at <a href="http://ideonomy.mit.edu/essays/traits.html" target="_blank" rel="nofollow">http://ideonomy.mit.edu/essays/traits</a> but a couple of the words might need removing.</p>
+			<h2>About Sander</h2>
+			<p>Sander lives in Perth and loves swing dancing and software development</p>
+			<p>
+				<a href="https://twitter.com/ahuijsen" target="_blank" rel="nofollow">
+					<Twitter />
+				</a>
+				<a href="http://sanderhuijsen.com/" target="_blank" rel="nofollow">
+					<Globe />
+				</a>
+			</p>
+			<h2>About Amy</h2>
+			<p>Amy is a freelance front end developer, starting her own business and working as an Evangalist for YOW! Conference.</p>
+			<p>She enjoys spending her time in the community, speaking at events and blogs in her spare time (about tech, the web and life).</p>
+			<p>
+				<a href="https://twitter.com/Amys_Kapers" target="_blank" rel="nofollow">
+					<Twitter />
+				</a>
+				<a href="https://amygoestoperth.com.au/" target="_blank" rel="nofollow">
+					<Globe />
+				</a>
+			</p>
+		</div>
+	</div>
+);
